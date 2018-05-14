e8182ede1abddc6b6781c716980fa2fac45079b1
Joel Subias <jsubias@24dcaea7d349>
2018-05-14 15:57:45 +0000

    Switched to using Flask-WTF for forms.

--- flask_app.py
+++ flask_app.py
@@ -7,12 +7,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import Navbar, Subgroup, View
-
+from flask_sslify import SSLify
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Email, Length
 app = Flask(__name__)
 
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
 Bootstrap(app)
+SSLify(app)
 nav = Nav(app)
 @nav.navigation('mysite_navbar')
 def create_navbar():
@@ -41,6 +45,15 @@ class Song(db.Model):
     artist_name = db.Column(db.String(80))
     youtube_url = db.Column(db.String(300))
 
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        'Username', validators=[InputRequired(), Length(min=4, max=15)])
+    email = StringField(
+        'Email', validators=[InputRequired(), Email()])
+    password = PasswordField(
+        'Password', validators=[InputRequired(), Length(min=8, max=80)])
+    submit = SubmitField('Register')
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
@@ -56,9 +69,15 @@ def class_schedule():
     return render_template('class_schedule.html',
                            courses=courses)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        return (
+            form.username.data + ', ' +
+            form.email.data + ', ' +
+            form.password.data)
+    return render_template('register.html', form=form)
 
 @app.route('/top_ten_songs')
 def top_ten_songs():
